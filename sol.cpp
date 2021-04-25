6e74f370de582023e9dd93c0dadd995ab5a57a14
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-04-25 11:15:25 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		// There are 'n' places you can put a vertical diamond, 
+		// so the answer is 'n' for each test case.
+		cout << n << '\n';
+	}
+	return 0;
+}
